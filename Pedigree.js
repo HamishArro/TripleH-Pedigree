fbecb67ebfa97656368eb705bb6ff22cb39f88c6
Hugh Cavanagh <hacavanagh@gmail.com>
2021-02-15 17:30:40 +0000

    added nottoEqual and tried writing not

--- Pedigree.js
+++ Pedigree.js
@@ -3,13 +3,29 @@ const referee = (actual) => ({
   toEqual: (expected) => {
     if (actual === expected){ return "PASS" };
     return "FAIL";
-  },
+  }
 
-  toThrowError: (expectedError) => {
+  ,notToEqual: (expected) =>  {
+    if (actual === expected){ return "FAIL" };
+    return "PASS";
+  }
+
+  ,toThrowError: (expectedError) => {
     try { actual()}
     catch(error) { if (expectedError === (error.message)) { return "PASS" };
       return "FAILED WRONG ERROR";
     }
-  return "FAILED";
+  return "FAIL";
   }
+
+  ,not: (matcher) => {
+    // console.log(this)
+    // console.log(referee.matcher)
+    // if(matcher === "PASS") {
+    //   return "FAIL"
+    // } else {
+    //   return "PASS"
+    // }
+  }
+
 })
