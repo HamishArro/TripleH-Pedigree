64ca66e963255097db6c9a70f888670e40e16ade
Hamish <hamisharro@icloud.com>
2021-02-15 17:04:11 +0000

    changed layout

--- Pedigree.js
+++ Pedigree.js
@@ -4,11 +4,10 @@ const referee = (actual) => ({
     if (actual === expected){ return "PASS" };
     return "FAIL";
   },
-  
+
   toThrowError: (expectedError) => {
     try { actual()}
-    catch(error) { if (expectedError === (error.message))
-      { return "PASS" };
+    catch(error) { if (expectedError === (error.message)) { return "PASS" };
       return "FAILED WRONG ERROR";
     }
   return "FAILED";
