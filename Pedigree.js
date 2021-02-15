847ca71109ad3992396c7d53177b5e8901cc9196
Hamish <hamisharro@icloud.com>
2021-02-15 17:03:36 +0000

    changed layout

--- Pedigree.js
+++ Pedigree.js
@@ -1,39 +1,16 @@
-// - to Equal /
-
-// function equalTo(a,b){
-//   if (a === b){
-//     return "PASS"
-//   }
-//   return "FAIL"
-// };
-
-//  assert(actual) => {
-//   toEqual: function(expected) {
-//     if (actual === expected){
-//       return "PASS";
-//     };
-//     return "FAIL";
-//   }
-// }
-
 const referee = (actual) => ({
+
   toEqual: (expected) => {
-    if (actual === expected){
-      return "PASS";
-    };
+    if (actual === expected){ return "PASS" };
     return "FAIL";
-  }
-
-
-  ,toThrowError: (expectedError) => {
-    try {
-      actual()
-    } catch(error) {
-      if (expectedError === (error.message)){
-        return "PASS";
-      };
+  },
+  
+  toThrowError: (expectedError) => {
+    try { actual()}
+    catch(error) { if (expectedError === (error.message))
+      { return "PASS" };
       return "FAILED WRONG ERROR";
-  }
+    }
   return "FAILED";
   }
 })
