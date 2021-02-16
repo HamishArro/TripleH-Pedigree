a4af7fad58ec95b43a39d4d9bd784cc5b3c278e7
Hamish <hamisharro@icloud.com>
2021-02-16 16:45:19 +0000

    add it and logic for each it

--- Pedigree.js
+++ Pedigree.js
@@ -1,25 +1,39 @@
-const referee = (actual) => ({
+var allTestLog = [];
+var testLog = [];
+
+const expect = (actual) => ({
 
   toEqual: (expected) => {
-    if (actual === expected){ return true };
-    return false;
-  }
+    console.log("inside toEqual");
+    if (actual === expected) { testLog.push(true) }
+    else { testLog.push(false) };
+  },
 
-  ,toThrowError: (expectedError) => {
+  toThrowError: (expectedError) => {
     try { actual()}
     catch(error) {
        if (expectedError === (error.message)) { return true };
       return error;
     }
   return false;
-  }
+  },
 
-  ,not: function(matcher) {
+  not: function(matcher) {
     this.f = new Function(`return this.${matcher}`)
-    returnValue = this.f()
+    returnValue = this.f();
     if(typeof returnValue === 'boolean') {
-      return !returnValue
+      return !returnValue;
     } else { return returnValue }
   }
 
 })
+
+const it = (description, test) => {
+
+  testLog = [];
+  console.log(testLog);
+  test();
+  console.log(testLog);
+  allTestLog[description] = testLog;
+
+}
---
+++ testTest.js
@@ -0,0 +1,5 @@
+it('creates a new test and checks if it passes', () => {
+  expect(2).toEqual(2);
+  expect(2).toEqual(2);
+  expect(2).toEqual(2);
+});
